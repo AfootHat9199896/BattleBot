6d2fb510b9365baa16272264d95b6a7e3e523bf3
Battle Chicken <felix@larkov.de>
2022-01-25 17:01:11 -0500

    Improved the deploy-commands so that it deletes unuses / commands

--- bot.js
+++ bot.js
@@ -4,18 +4,29 @@ const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+
+//Event Handler
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+console.log('-----Loading Events-----')
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+		console.log(event.name);
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+		console.log(event.name);
+	}
+}
+//Command Handler
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
+console.log('-----Loading Commands-----')
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.data.name, command);
+	console.log(command.data.name);
 }
-
-client.once('ready', () => {
-	console.log('Ready!');
-});
-
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
@@ -29,6 +40,7 @@ client.on('interactionCreate', async interaction => {
 		console.error(error);
 		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
+
 });
 
 client.login(token);
