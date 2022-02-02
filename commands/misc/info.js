18b7136f6749afe78d4355277afd359a3ac67b64
Battle Chicken <felix@larkov.de>
2022-02-02 07:39:53 -0500

    added info command

--- commands/misc/info.js
+++ commands/misc/info.js
@@ -2,13 +2,14 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	category: "category-here", //current available categories [misc, music, fun]
-	usage: "/example",   //use <> to indicate arguments, ie /play <song-name>
+	usage: "/info",   //use <> to indicate arguments, ie /play <song-name>
 	data:
 		new SlashCommandBuilder()
-		.setName('example')
-		.setDescription('Describes the function of the command'),
+		.setName('info')
+		.setDescription('Gives information about the server'),
 	async execute(interaction) {
-		//put code here
+		
+       await interaction.reply("") 
 	},
 };
 
