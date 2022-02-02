8d89c3eefb842de798b7b1573788e0f4f4b60ff7
AfootHat9199896 <alananthonymorales@gmail.com>
2022-02-02 18:45:21 -0500

    Added improved hi function randomizer

--- commands/misc/hi.js
+++ commands/misc/hi.js
@@ -7,43 +7,9 @@ module.exports = {
 		.setName('hi')
 		.setDescription('Replies with Howdy!'),
 	async execute(interaction) {
-		x = Math.floor(Math.random() * 10) 
-switch (x) {
-    case 1:
-        console.log("howdy")
-        break;
-    case 2:
-        console.log("hi")
-        break;
-    case 3:
-        console.log("yoo")
-        break;
-    case 4:
-        console.log("sup")
-        break;
-    case 5:
-        console.log("howdy parn'r")
-        break;
-    case 6:
-        console.log("hey")
-        break;
-    case 7:
-        console.log("hiiiii")
-        break;
-    case 8:
-        console.log("ello")
-        break;
-    case 9:
-        console.log("helllllo")
-        break;
-    case 10:
-        console.log("bye")
-        break;
-    
-    
-
-    default:
-        break;
+        await interaction.reply(responses[x])
+        x = Math.floor(Math.random() * 10) 
+        await interaction.reply(responses[x])
 }
 
 		
