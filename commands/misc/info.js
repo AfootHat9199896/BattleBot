abf2a8ae7bca526473fa737bc3321c73ae3d37c9
Battle Chicken <felix@larkov.de>
2022-02-02 15:52:30 -0500

    i

--- commands/misc/info.js
+++ commands/misc/info.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
+const { MessageActionRow, MessageButton } = require('discord.js');
 module.exports = {
 	category: "category-here", //current available categories [misc, music, fun]
 	usage: "/info",   //use <> to indicate arguments, ie /play <song-name>
@@ -8,8 +8,19 @@ module.exports = {
 		.setName('info')
 		.setDescription('Gives information about the server'),
 	async execute(interaction) {
+		const row = new MessageActionRow()
+		.addComponents(
+			new MessageButton()
+				.setCustomId('ping')
+				.setLabel('Ping')
+				.setStyle('PRIMARY'),
+			new MessageButton()
+				.setCustomId("population")
+				.setLabel("Population")
+				.setStyle("PRIMARY")
+		)
 		
-       await interaction.reply("") 
+       await interaction.reply({content: "Choose what info you want to seee.", components: [row]}) 
 	},
 };
 
