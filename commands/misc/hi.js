9972eac2a39bb9c261a0da945df865801bf0a5a4
AfootHat9199896 <alananthonymorales@gmail.com>
2022-02-02 19:07:41 -0500

    Improved "Hi command"

--- commands/misc/hi.js
+++ commands/misc/hi.js
@@ -7,10 +7,13 @@ module.exports = {
 		.setName('hi')
 		.setDescription('Replies with Howdy!'),
 	async execute(interaction) {
+
+        const responses = ["howdy", "hi", "yoo", "sup", "howdy parn'r", "hey", "hiiiii", "ello", "helllllo","bye"]
+        x = Math.floor(Math.random() * 10)
         await interaction.reply(responses[x])
-        x = Math.floor(Math.random() * 10) 
-        await interaction.reply(responses[x])
-}
+ 
+
+
 
 		
 	},
